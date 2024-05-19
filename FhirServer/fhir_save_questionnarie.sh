26bf8c7b5387ba1ef9281a6fff9463b193de32b7
J4V1R6 <75162620+J4V1R6@users.noreply.github.com>
2024-05-19 11:44:02 +0200

    Implementación servidor Fhir R5 local en contenedor Docker

---
+++ FhirServer/fhir_save_questionnarie.sh
@@ -0,0 +1,1 @@
+curl -X POST -H 'Content-Type: application/json' -d @questionnaire_fhir.json http://localhost:8080/fhir/Questionnaire
---
+++ fhirServer_start.sh
@@ -0,0 +1,31 @@
+cd ./FhirServer
+
+# Arranca el servidor Fhir local que se utiliza (R5)
+echo "Arrancando el contenedor docker con el servidor Fhir local"
+sudo docker compose up -d
+
+# Se da un tiempo a que arranque
+echo "Esperando 50 segundos a que arranque el servidor"
+echo 0
+sleep 10
+
+echo 10
+sleep 10
+
+echo 20
+sleep 10
+
+echo 30
+sleep 10
+
+echo 40
+sleep 10
+
+# Sube el cuestionario que permite realizar la solicitud de consentimientos
+echo "Publicando el recurso Questionnaire en el servidor Fhir local"
+./fhir_save_questionnarie.sh
+
+# Arranca la aplicación Gestor de Consentimientos
+echo "Arrancando aplicación Gestor de Consentimientos"
+cd ../gestorconsentimientos-service/
+./launch.sh clean install
